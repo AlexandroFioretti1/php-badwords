8aa5b72b02325f71f41f599ea2e4695847509bdd
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-05-03 17:11:25 +0200

    create form and add php resault

--- index.php
+++ index.php
@@ -1,22 +1,32 @@
-<?php
-
-$name = "Alexandro";
-
-echo $name;
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php</title>
-</head>
-<body>
-    
-<h1>ciao mi chiamo <?php echo $name; ?></h1>
 
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>php badwords</title>
+  <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
+</head>
+
+<body>
+  <div class="container d-flex justify-content-center">
+    <div class="card rounded-0 shadow col-8">
+      <div class="card-body">
+        <form action="myscript.php" method="get">
+          <div class="mb-3">
+            <label for="frase">Testo libero</label>
+            <input type="text" name="frase" id="frase" class="form-control" placeholder="Inserisci una frase liberatoria" aria-describedby="suffixId">
+          </div>
+          <div class="mb-3">
+            <label for="censura">Parola da censura</label>
+            <input type="text" name="censura" id="censura" class="form-control" placeholder="scegli la/le parola/e da censurare" aria-describedby="suffixId">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="reset" class="btn btn-primary">Reset</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
--- myscript.php
+++ myscript.php
@@ -1,7 +1,27 @@
 <?php
-
-$name = "";
-
-echo $name;
-
+$frase = trim($_GET["frase"]);
+$censura =trim($_GET["censura"]);
+$censurata =str_replace($censura,"***",$frase)
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php badwords resaults</title>
+</head>
+<body>
+  <div class="container d-flex justify-content-center">
+    <div class="card rounded-0 shadow col-8">
+      <div class="card-body">
+        <p>La tua frase è : <?php echo $frase ?> </p>
+        <p>la frase intera è lunga : <?php echo strlen($frase)?></p>
+        <p>questa è la frase censurata : <?php echo $censurata ?></p>
+        <p>la frase censurata è lunga : <?php echo strlen($censurata)?></p>
+      </div>
+    </div>
+  </div>
+</body>
+</html>

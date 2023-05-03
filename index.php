2d6fffa6b506da7f8c25f1b834d563154641011e
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-05-03 15:49:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$name = "Alexandro";
+
+echo $name;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+    
+<h1>ciao mi chiamo <?php echo $name; ?></h1>
+
+</body>
+</html>
